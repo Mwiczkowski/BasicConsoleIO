d9816bab44052af2568622991817d43186899f48
Mwiczkowski <miloszwiczkowski@gmail.com>
2021-08-20 11:33:23 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasicConsoleIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Set console title
+            Console.Title = "Get user information";
+
+            //Get name and age
+            Console.Write("Introduce your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Introduce your age: ");
+            string userAge = Console.ReadLine();
+
+            //Set console text color
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            //3 methods of displaying the text
+            Console.WriteLine("Hello " + userName + "! You are " + userAge +  " years old.");
+            Console.WriteLine($"Hello {userName}! You are {userAge} years old.");
+            Console.WriteLine("Hello {0}! You are {1} years old.", userName, userAge);
+
+            //Reset to default console color for all the text after this code
+            Console.ResetColor();
+            
+        }
+    }
+}
